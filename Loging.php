62377ad07efd21c3cc19cbfc255e2284dc54055d
dumidu1998 <dumiduraj@gmail.com>
2021-10-11 19:00:23 +0530

    home done

--- Loging.php
+++ Loging.php
@@ -1,47 +1,46 @@
 <?php
 
 require 'Php/connection.php';
+$error = "";
+$mailid = "";
+$password = "";
+
+if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+    $mailid = $_POST["emailid"];
+    $password = $_POST["password"];
 
 
-if ($_SERVER["REQUEST_METHOD"] == 'POST'){
-
-    $mailid = $_POST["emailid"]; 
-    $password = $_POST["Password"];
-    
-
-    if(empty($mailid)|| empty($password)){
+    if (empty($mailid) || empty($password)) {
         header("Location:Loging.php?error=emptyfields");
         exit();
-    }else{
+    } else {
 
-        $sql = "SELECT * FROM  users WHERE user_email='$mailid' AND user_password='$password'";
-
-        echo $sql;
+        $sql = "SELECT * FROM  user WHERE email='$mailid' AND password='" . md5($password) . "'";
 
         $result = $conn->query($sql);
 
-        print_r($result);
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                session_start();
+                $_SESSION['user_id'] = $row['id'];
+                $_SESSION['username'] = $row['username'];
+                $_SESSION['email'] = $row['email'];
 
-        if($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) {
-                    session_start();
-                    $_SESSION['user_id']=$row['user_id'];
-                    $_SESSION['user_name']=$row['user_uname'];
-                    $_SESSION['user_email']=$row['user_email'];
-
-
-                    header("Location:http://localhost/finalProject/index.php?success=true");
-                    exit();
+                header("Location: /Foodsysterm/index.php?logged=true");
+                $cookie_name = "logged";
+                $cookie_value = true;
+                setcookie($cookie_name, $cookie_value, time() + (86400), "/");
+                exit();
             }
         } else {
-            header("Location:Loging.php?success=false");
+            // header("Location:Loging.php?success=false");
+            $error = "Invalid Email or Password";
         }
 
         $conn->close();
-         
     }
 }
-    
+
 ?>
 
 <html lang="en">
@@ -57,7 +56,7 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST'){
         .cls-heading {
             text-align: center;
         }
-        
+
         .my-shadow {
             box-shadow: 9px 4px 14px -3px rgba(87, 82, 82, 0.75);
             -webkit-box-shadow: 9px 4px 14px -3px rgba(87, 82, 82, 0.75);
@@ -68,13 +67,13 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST'){
 </head>
 
 <body>
-    
+
     <section class="vh-100">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-sm-6 text-black">
 
-                  
+
                     <div class="d-flex align-items-center h-custom-2 px-6 ms-xl-5 mt-5 pt-5 pt-xl-0 mt-xl-n5">
 
                         <form action="Loging.php" method="POST" style="width: 23rem;">
@@ -82,22 +81,24 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST'){
                             <h3 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Log in</h3>
 
                             <div class="form-outline mb-4">
-                                <input type="email" id="form2" name="emailid" class="form-control form-control-lg" />
                                 <label class="form-label" for="form2">Email address</label>
+                                <input type="email" id="form2" value="<?php echo $mailid; ?>" name="emailid" class="form-control form-control-lg" />
                             </div>
 
                             <div class="form-outline mb-4">
-                                <input type="password" id="form3" name="password" class="form-control form-control-lg" />
                                 <label class="form-label" for="form3">Password</label>
+                                <input type="password" id="form3" value="<?php echo $password; ?>" name="password" class="form-control form-control-lg" />
                             </div>
-
                             <div class="pt-1 mb-4">
                                 <button class="btn btn-primary" type="submit">Login</button>
-                                <a href="index.php" class="btn btn-danger">Back</a>
+                                <!-- <a href="index.php" class="btn btn-danger">Back</a> -->
+                                <div class="text-danger">
+                                    <?php echo $error ?>
+                                </div>
                             </div>
 
                             <p class="small mb-5 pb-lg-2"><a class="text-muted" href="Foget password.html">Forgot password?</a></p>
-                            <p>Don't have an account? <a href="Sign in.html" class="link-info">Register here</a></p>
+                            <p>Don't have an account? <a href="signup.php" class="link-info">Register here</a></p>
 
                         </form>
 
@@ -111,7 +112,7 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST'){
         </div>
     </section>
     </div>
-     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </body>
 
